04eb4b33428bcfdf3675014f493a34a932b05c52
admin <466568905@qq.com>
2021-10-23 17:59:51 +0800

    full screen test

--- src/components/test_01_FullScreen.vue
+++ src/components/test_01_FullScreen.vue
@@ -1,12 +1,53 @@
 <template>
     <div>
-        <h1>test 01</h1>
+        <button type="button" @click="switchFullScreen">fullscreen</button>
+        <textarea ref="htmlEditor"></textarea>
     </div>
 </template>
 
 <script>
+    import 'codemirror/lib/codemirror.css';
+    import codemirror from "codemirror";
+
+    import 'codemirror/theme/eclipse.css';
+    import 'codemirror/mode/htmlmixed/htmlmixed';
+
+    import 'codemirror/addon/display/fullscreen.css';
+    import 'codemirror/addon/display/fullscreen';
+
     export default {
-        name: "test_01_FullScreen"
+        name: "test_01_FullScreen",
+        data:function () {
+            return {
+                htmlEditor:'',
+                htmlOptions:{
+                    autofocus:true,
+                    lineNumbers: true,
+                    line:true,
+                    tabSize:4,
+                    mode:'htmlmixed',
+                    theme:'eclipse',
+                    extraKeys: {
+                        "F11": function(cm) {
+                            cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+                        },
+                        "Esc": function(cm) {
+                            if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+                        }
+                    }
+                },
+            }
+        },
+        methods:{
+            switchFullScreen(){
+                let cm = this.htmlEditor;
+                cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+                cm.focus();
+            }
+        },
+        mounted:function () {
+            this.htmlEditor = codemirror.fromTextArea(this.$refs.htmlEditor,this.htmlOptions);
+        }
     }
 </script>
 
